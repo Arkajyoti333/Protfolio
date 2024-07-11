eaafeac9f848b44f08374255466361e02d60875d
Swarnendu Mondal <swarnendumondal2012@gmail.com>
2024-07-11 23:01:30 +0530

    file

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const about = () => {
+const home = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const about = () => {
   )
 }
 
-export default about
+export default home
