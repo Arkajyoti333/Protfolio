0db1f1392174297144850f5cfef26fd633522bd3
Arkajyoti Kundu <arkajyotikundu415@gmail.com>
2024-07-11 21:14:27 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import NavBar from "./components/NavBar/NavBar";
+
+const  App=()=> {
+  return (
+    <>
+      <div>
+     
+        <p className='font-extrabold'>
+          Arkajyoti
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <>
+      <div>
+        
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
